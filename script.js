0e7f1642b0a2d76c15509d54bc96efaaad672da2
amaralmillena-coder <amaral.millena@escola.pr.gov.br>
2026-06-10 07:51:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    alert("Mensagem enviada com sucesso!");
+
+    form.reset();
+});
